fc8745d8556a3cb0809c194b0805be912d0aa173
Victor Hugo <136921549+wilyJ80@users.noreply.github.com>
2023-08-21 20:51:59 +0000

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,24 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		-- amongst your other plugins
+		--{'akinsho/toggleterm.nvim', version = "*", config = true}
+		-- or
+		{
+			'akinsho/toggleterm.nvim',
+			version = "*",
+			opts = {
+				size = 10,
+				open_mapping = [[<F12>]],
+				shading_factor = 2,
+				direction = "float",
+				float_opts = {
+					border = "curved",
+					highlights = { border = "Normal", background = "Normal" },
+				},
+			}
+		}
+	}
+}
