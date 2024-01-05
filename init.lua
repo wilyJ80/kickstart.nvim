5bf3e9bdd8be0f1c96468580644ce7df25f54bcb
wilyJ80 <abjurandam@gmail.com>
2024-01-05 16:41:48 -0300

    Add more languages for treesitter

--- init.lua
+++ init.lua
@@ -317,7 +317,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'lua', 'python', 'vimdoc', 'vim', 'java', 'bash' },
+  ensure_installed = { 'c', 'lua', 'python', 'java', 'bash', 'javascript', 'css', 'csv', 'dockerfile', 'dot', 'html', 'markdown' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
