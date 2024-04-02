df9af62f6c9101d280f8df8f593943b9a4a2e107
wilyJ80 <abjurandam@gmail.com>
2024-04-02 19:09:23 -0300

    Updates to theme

--- init.lua
+++ init.lua
@@ -210,7 +210,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-vim.opt.background = "dark"
+vim.opt.background = "light"
 
 -- Set highlight on search
 vim.o.hlsearch = false
