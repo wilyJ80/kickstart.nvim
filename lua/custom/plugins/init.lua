911e4c8f5ddfb633babbd3a8788aa2b955a902bd
Victor Hugo <abjurandam@gmail.com>
2024-05-26 12:03:23 -0300

    Update toggleterm config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,14 +8,8 @@ return {
     version = '*',
     config = true,
     opts = {
-      size = 10,
       open_mapping = [[<F12>]],
-      shading_factor = 2,
       direction = 'float',
-      float_opts = {
-        border = 'curved',
-        highlights = { border = 'Normal', background = 'Normal' },
-      },
     },
   },
 }
