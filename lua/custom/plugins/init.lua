dd9072bf8a96a44b55d158ba74e9157e6bb98a51
Victor Hugo <abjurandam@gmail.com>
2024-05-27 13:01:11 -0300

    Fix syntax error

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,25 +11,25 @@ return {
       open_mapping = [[<F12>]],
       direction = 'float',
     },
-    {
-      'karb94/neoscroll.nvim',
-      mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-      hide_cursor = true,
-      stop_eof = true,
-      respect_scrollof = false,
-      cursos_scrolls_alone = true,
-      easing_function = nil,
-      pre_hook = nil,
-      post_hook = nil,
-      performance_mode = false,
-      opts = {},
-    },
+  },
+  {
+    'karb94/neoscroll.nvim',
+    mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+    hide_cursor = true,
+    stop_eof = true,
+    respect_scrollof = false,
+    cursos_scrolls_alone = true,
+    easing_function = nil,
+    pre_hook = nil,
+    post_hook = nil,
+    performance_mode = false,
+    opts = {},
+  },
 
-    {
-      'mfussenegger/nvim-jdtls',
-    },
-    {
-      'norcalli/nvim-colorizer.lua',
-    },
+  {
+    'mfussenegger/nvim-jdtls',
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
   },
 }
